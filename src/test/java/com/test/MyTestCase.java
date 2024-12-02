51109ee91b0e644fadb263ec3d43fdc9d818f6ee
rpar <p_ravis@yahoo.com>
2024-12-01 20:38:32 -0800

    added maven build

--- src/test/java/com/test/MyTestCase.java
+++ src/test/java/com/test/MyTestCase.java
@@ -1,7 +1,7 @@
 package com.test;
 
-import org.junit.Test;
-//import org.junit.jupiter.api.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.tryout.MyTryout;
 
@@ -11,6 +11,7 @@ public class MyTestCase {
 	public void testOptional() {
 		//MyTryout myTryout = new MyTryout();
 		
+		System.out.println("Starting");
 		boolean asserted = MyTryout.checkOptionNullable("String");
 		
 		assert(asserted);
